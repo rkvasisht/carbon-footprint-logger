7e289bdec5e9dbd9fb708d941ca179953b01c322
rkvasisht <rkvasisht@gmail.com>
2018-06-27 16:59:49 -0700

    abble to query api and get carbon footprint for total distance of vehicle.id

--- controllers/carinfo.js
+++ controllers/carinfo.js
@@ -37,22 +37,29 @@ router.get('/:id', isLoggedIn, function(req, res){
       var make = vehicle.make;
       var model = vehicle.model;
       var year = vehicle.year;
-      var distance = milage[0].distance;
-      console.log(make);
-      console.log(model);
-      console.log(year);
-      console.log(distance);
+      var totalDistanceArray = [];
+      milage.forEach(function(distance){
+        var dist = distance.distance
+        totalDistanceArray.push(parseInt(dist));
+      })
+      var totalDistanceArrayReduce = totalDistanceArray.reduce((total, ammount) => total + ammount);
+      var distanceTraveled = totalDistanceArrayReduce/0.621371;
 
 
-      request.post('http://impact.brighterplanet.com/automobiles?daily_distance='+distance+'&make='+make+'&model='+model+'&timeframe=2018-01-01%2F2019-01-01&year='+year+'',
-                  {
+
+      request.post('http://impact.brighterplanet.com/automobiles.json?daily_distance='+distanceTraveled+'&make='+make+'&model='+model+'&timeframe=2018-01-01%2F2019-01-01&year='+year+'',
+            {
                   key:'207b6b0f-de5d-40ee-a4d9-4d0a9103c4fc'}, function(error, response, body){
-                    console.log(body);
+                    var footprintData = JSON.parse(body);
+                    var footprintAmount = footprintData.decisions.carbon.description
+                    var footprintAmountDailyInTons = ((footprintAmount.match(/\d+/)[0])*2.20462/365).toFixed(2);
+                    console.log(footprintAmountDailyInTons);
+                    // console.log(footprintAmount);
+
+
                   })
 
     })
-
-
     });
   });
 
